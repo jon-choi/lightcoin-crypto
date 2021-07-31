9a56847ed853f84e769e2fee9163af4170629532
Jonathan Choi <jonchoi11@gmail.com>
2021-07-31 05:25:14 +0000

    finished

--- index.js
+++ index.js
@@ -1,15 +1,59 @@
-let balance = 500.00;
+class Account {
 
-class Withdrawal {
+  constructor(username) {
+    this.username = username;
+    this.transactions = [];
+  }
 
-  constructor(amount) {
+  get balance() {
+    let balance = 0;
+    for (let t of this.transactions) {
+      balance += t.value;
+    }
+    return balance;
+
+  }
+
+  addTransaction(transaction) {
+    this.transactions.push(transaction);
+  }
+}
+
+class Transaction {
+
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    if (!this.isAllowed()) return false;
+    this.time = new Date();
+    this.account.addTransaction(this);
+    return true;
+  }
+}
+
+class Withdrawal extends Transaction {
+
+  get value() {
+    return -this.amount;
   }
 
+  isAllowed() {
+    return (this.account.balance - this.amount >= 0);
+  }
+}
+
+class Deposit extends Transaction {
+
+  get value() {
+    return this.amount;
+  }
+
+  isAllowed() {
+    return true;
+  }
 }
 
 
@@ -17,13 +61,29 @@ class Withdrawal {
 
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
+const myAccount = new Account('snow-patrol');
 
-t1 = new Withdrawal(50.25);
+console.log('Starting Balance:', myAccount.balance);
+
+console.log('My first paycheque!');
+const t1 = new Deposit(600.00, myAccount);
 t1.commit();
-console.log('Transaction 1:', t1);
+console.log('Account Balance:', myAccount.balance);
 
-t2 = new Withdrawal(9.99);
+console.log('Fancy dinner with the girlfriend')
+const t2 = new Withdrawal(150.00, myAccount);
 t2.commit();
-console.log('Transaction 2:', t2);
+console.log('Account Balance:', myAccount.balance);
 
-console.log('Balance:', balance);
+console.log('Birthday Money!');
+const t3 = new Deposit(1500.00, myAccount);
+t3.commit();
+console.log('Account Balance:', myAccount.balance);
+
+console.log('New transmission for the car :(');
+const t4 = new Withdrawal(1200.00, myAccount);
+t4.commit();
+
+console.log('Ending Balance:', myAccount.balance);
+
+console.log('Account Transaction History:', myAccount.transactions);
